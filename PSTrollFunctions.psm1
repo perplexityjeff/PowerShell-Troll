8e8a112db1f3558db0bd7e5065d539d2006f331f
Jeffrey Jansma <me@perplexity.nl>
2017-09-15 22:05:52 +0200

    Add files via upload

--- PSTrollFunctions.psm1
+++ PSTrollFunctions.psm1
@@ -1,3 +1,4 @@
+#HELPER FUNCTIONS
 Function Start-AudioControl {
 Add-Type -TypeDefinition '
 using System.Runtime.InteropServices;
@@ -51,6 +52,17 @@ public class Audio
 '
 }
 
+Function b($a,$b){
+    [console]::beep($a,$b)
+}
+
+Function s($a){
+    sleep -m $a
+}
+
+
+
+#MAIN FUNCTIONS
 Function Set-AudioMax {
     Start-AudioControl
     [audio]::Volume = 1
@@ -75,7 +87,6 @@ Function Set-AudioLevel {
 Function Start-CatFact 
 {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    Add-Type -AssemblyName System.speech
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
     $CatFact = Invoke-RestMethod -Uri 'https://catfact.ninja/fact' -Method Get | Select-Object -ExpandProperty fact
     $SpeechSynth.Speak("did you know?")
@@ -89,7 +100,6 @@ Function Start-RickRoll
 
 Function Start-RowBoat 
 {
-    Add-Type -AssemblyName System.speech
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
     $SpeechSynth.Speak("Row, Row, Row your boat gently down the stream.  Merrily! Merrily! Merrily! Life is but a dream.")    
 }
@@ -113,7 +123,363 @@ Function Start-NotificationSoundSpam
 
 Function Send-VoiceMessage([string]$Message)
 {
-    Add-Type -AssemblyName System.speech
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
     $SpeechSynth.Speak($Message)
 }
+
+Function Send-SuperMario
+{
+    b 660 100;
+    s 150;
+    b 660 100;
+    s 300;
+    b 660 100;
+    s 300;
+    b 510 100;
+    s 100;
+    b 660 100;
+    s 300;
+    b 770 100;
+    s 550;
+    b 380 100;
+    s 575;
+
+    b 510 100;
+    s 450;
+    b 380 100;
+    s 400;
+    b 320 100;
+    s 500;
+    b 440 100;
+    s 300;
+    b 480 80;
+    s 330;
+    b 450 100;
+    s 150;
+    b 430 100;
+    s 300;
+    b 380 100;
+    s 200;
+    b 660 80;
+    s 200;
+    b 760 50;
+    s 150;
+    b 860 100;
+    s 300;
+    b 700 80;
+    s 150;
+    b 760 50;
+    s 350;
+    b 660 80;
+    s 300;
+    b 520 80;
+    s 150;
+    b 580 80;
+    s 150;
+    b 480 80;
+    s 500;
+
+    b 510 100;
+    s 450;
+    b 380 100;
+    s 400;
+    b 320 100;
+    s 500;
+    b 440 100;
+    s 300;
+    b 480 80;
+    s 330;
+    b 450 100;
+    s 150;
+    b 430 100;
+    s 300;
+    b 380 100;
+    s 200;
+    b 660 80;
+    s 200;
+    b 760 50;
+    s 150;
+    b 860 100;
+    s 300;
+    b 700 80;
+    s 150;
+    b 760 50;
+    s 350;
+    b 660 80;
+    s 300;
+    b 520 80;
+    s 150;
+    b 580 80;
+    s 150;
+    b 480 80;
+    s 500;
+
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 150;
+    s 300;
+    b 380 100;
+    s 150;
+    b 430 100;
+    s 150;
+
+    b 500 100;
+    s 300;
+    b 430 100;
+    s 150;
+    b 500 100;
+    s 100;
+    b 570 100;
+    s 220;
+
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 200;
+    s 300;
+
+    b 1020 80;
+    s 300;
+    b 1020 80;
+    s 150;
+    b 1020 80;
+    s 300;
+
+    b 380 100;
+    s 300;
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 150;
+    s 300;
+    b 380 100;
+    s 150;
+    b 430 100;
+    s 150;
+
+    b 500 100;
+    s 300;
+    b 430 100;
+    s 150;
+    b 500 100;
+    s 100;
+    b 570 100;
+    s 420;
+
+    b 585 100;
+    s 450;
+
+    b 550 100;
+    s 420;
+
+    b 500 100;
+    s 360;
+
+    b 380 100;
+    s 300;
+    b 500 100;
+    s 300;
+    b 500 100;
+    s 150;
+    b 500 100;
+    s 300;
+
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 150;
+    s 300;
+    b 380 100;
+    s 150;
+    b 430 100;
+    s 150;
+
+    b 500 100;
+    s 300;
+    b 430 100;
+    s 150;
+    b 500 100;
+    s 100;
+    b 570 100;
+    s 220;
+
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 200;
+    s 300;
+
+    b 1020 80;
+    s 300;
+    b 1020 80;
+    s 150;
+    b 1020 80;
+    s 300;
+
+    b 380 100;
+    s 300;
+    b 500 100;
+    s 300;
+
+    b 760 100;
+    s 100;
+    b 720 100;
+    s 150;
+    b 680 100;
+    s 150;
+    b 620 150;
+    s 300;
+
+    b 650 150;
+    s 300;
+    b 380 100;
+    s 150;
+    b 430 100;
+    s 150;
+
+    b 500 100;
+    s 300;
+    b 430 100;
+    s 150;
+    b 500 100;
+    s 100;
+    b 570 100;
+    s 420;
+
+    b 585 100;
+    s 450;
+
+    b 550 100;
+    s 420;
+
+    b 500 100;
+    s 360;
+
+    b 380 100;
+    s 300;
+    b 500 100;
+    s 300;
+    b 500 100;
+    s 150;
+    b 500 100;
+    s 300;
+
+    b 500 60;
+    s 150;
+    b 500 80;
+    s 300;
+    b 500 60;
+    s 350;
+    b 500 80;
+    s 150;
+    b 580 80;
+    s 350;
+    b 660 80;
+    s 150;
+    b 500 80;
+    s 300;
+    b 430 80;
+    s 150;
+    b 380 80;
+    s 600;
+
+    b 500 60;
+    s 150;
+    b 500 80;
+    s 300;
+    b 500 60;
+    s 350;
+    b 500 80;
+    s 150;
+    b 580 80;
+    s 150;
+    b 660 80;
+    s 550;
+
+    b 870 80;
+    s 325;
+    b 760 80;
+    s 600;
+
+    b 500 60;
+    s 150;
+    b 500 80;
+    s 300;
+    b 500 60;
+    s 350;
+    b 500 80;
+    s 150;
+    b 580 80;
+    s 350;
+    b 660 80;
+    s 150;
+    b 500 80;
+    s 300;
+    b 430 80;
+    s 150;
+    b 380 80;
+    s 600;
+
+    b 660 100;
+    s 150;
+    b 660 100;
+    s 300;
+    b 660 100;
+    s 300;
+    b 510 100;
+    s 100;
+    b 660 100;
+    s 300;
+    b 770 100;
+    s 550;
+    b 380 100;
+    s 575;
+}
+
+
