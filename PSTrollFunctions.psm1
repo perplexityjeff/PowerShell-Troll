d9e2dc2c6716096fe6bab0b39f76c2f36b859ceb
Jeffrey Jansma <me@perplexity.nl>
2017-11-10 09:17:41 +0100

    Redone the CD Tray opening and added the closing
    Based on this tip:
    
    http://www.powershellmagazine.com/2013/11/12/pstip-ejecting-and-closing-cdrom-drive-the-powershell-way/

--- PSTrollFunctions.psm1
+++ PSTrollFunctions.psm1
@@ -109,11 +109,67 @@ Function Send-Message([string]$Message)
     msg.exe * $Message
 }
 
-Function Open-CDDrive{
-    $sh = New-Object -ComObject "Shell.Application"
-    $sh.Namespace(17).Items() | 
-        Where-Object { $_.Type -eq "CD Drive" } | 
-            foreach { $_.InvokeVerb("Eject") }
+Function Open-CDDrive
+{
+    Add-Type -TypeDefinition  @'
+    using System;
+    using System.Runtime.InteropServices;
+    using System.ComponentModel;
+     
+    namespace CDROM
+    {
+        public class Commands
+        {
+            [DllImport("winmm.dll")]
+            static extern Int32 mciSendString(string command, string buffer, int bufferSize, IntPtr hwndCallback);
+     
+            public static void Eject()
+            {
+                 string rt = "";
+                 mciSendString("set CDAudio door open", rt, 127, IntPtr.Zero);
+            }
+     
+            public static void Close()
+            {
+                 string rt = "";
+                 mciSendString("set CDAudio door closed", rt, 127, IntPtr.Zero);
+            }
+        }
+    }  
+'@
+
+[CDROM.Commands]::Eject()
+}
+
+Function Close-CDDrive{
+    Add-Type -TypeDefinition  @'
+    using System;
+    using System.Runtime.InteropServices;
+    using System.ComponentModel;
+     
+    namespace CDROM
+    {
+        public class Commands
+        {
+            [DllImport("winmm.dll")]
+            static extern Int32 mciSendString(string command, string buffer, int bufferSize, IntPtr hwndCallback);
+     
+            public static void Eject()
+            {
+                 string rt = "";
+                 mciSendString("set CDAudio door open", rt, 127, IntPtr.Zero);
+            }
+     
+            public static void Close()
+            {
+                 string rt = "";
+                 mciSendString("set CDAudio door closed", rt, 127, IntPtr.Zero);
+            }
+        }
+    }  
+'@
+
+[CDROM.Commands]::Close()
 }
 
 Function Start-NotificationSoundSpam
