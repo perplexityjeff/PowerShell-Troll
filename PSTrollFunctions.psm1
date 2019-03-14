8ce2d828477e2858f9a329e1bc963a6848090367
Jeffrey Jansma <me@perplexity.nl>
2019-03-14 09:16:19 +0100

    Added Happy Birthday but console beeps on a session hmmm?
    Added Happy Birthday but console beeps on a session hmmm?

--- PSTrollFunctions.psm1
+++ PSTrollFunctions.psm1
@@ -668,4 +668,39 @@ Function Send-SuperMario
     s 575;
 }
 
+Function Send-HappyBirthday
+{
+    $BeepList = @(
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1059.274; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1334.601; Length = 950; };
 
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1059.274; Length = 500; };
+    @{ Pitch = 1587.117; Length = 500; };
+    @{ Pitch = 1413.961; Length = 950; };
+
+    @{ Pitch = 1059.274; Length = 300; };
+    @{ Pitch = 1059.274; Length = 200; };
+    @{ Pitch = 2118.547; Length = 500; };
+    @{ Pitch = 1781.479; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1334.601; Length = 500; };
+    @{ Pitch = 1188.995; Length = 500; };
+    @{ Pitch = 1887.411; Length = 300; };
+    @{ Pitch = 1887.411; Length = 200; };
+    @{ Pitch = 1781.479; Length = 500; };
+    @{ Pitch = 1413.961; Length = 500; };
+    @{ Pitch = 1587.117; Length = 500; };
+    @{ Pitch = 1413.961; Length = 900; };
+    );
+
+    foreach ($Beep in $BeepList) {
+        [System.Console]::Beep($Beep['Pitch'], $Beep['Length']);
+    }
+}
